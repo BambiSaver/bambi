3b19b6a18695e7ec66a26f3314bc9ae2b3cac35f
florian-world <m@florian.world>
2018-08-06 21:25:35 +0200

    Improved dev setup script

--- setup_bambi_dev_environment.sh
+++ setup_bambi_dev_environment.sh
@@ -1,16 +1,43 @@
 #!/usr/bin/env bash
 
+echo "##############################################"
+echo " SCRIPT WILL FINISH WITH catkin build"
+echo " script asks for root privileges for apt-get"
+echo "##############################################"
+
+echo
+
+
+# checkout all submodules
 git submodule update --init --recursive
 
+
+# init catkin workspace
 catkin init
 wstool init src
 
+# install stuff
 sudo apt-get install python-catkin-tools -y
-
 rosdep install --from install --from-paths src --ingor-src -y
-
 src/mavros/install_geographiclib_datasets.sh
 
+
+# ADD TO BASHRC
+path="$(pwd)/devel/setup.bash"
+
+# assume to under home directory and replacing /home/$USER/ with ~/
+path=${path#*/}
+path=${path#*/}
+path="${path#*/}"
+
+if [ $(cat ~/.bashrc | grep -c $path) -lt 1 ] ; then
+  echo "source ~/$path" >> ~/.bashrc;
+fi
+
+
+#build
+catkin config -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER_ARG1=-std=c++11 -D__cplusplus=201103L -D__GXX_EXPERIMENTAL_CXX0X__=1
 catkin build
 
-echo "source devel/setup.bash" >> ~/.bashrc
+# even
+source devel/setup.bash
