83af455dd9ef0b33282c8584fac417e0953427c2
mick96 <michael.rimondi1996@gmail.com>
2018-08-23 17:31:32 +0200

    Updated yi trigger script in optical_cam node (from rasPI)

--- src/bambi/bambi/scripts/optical_cam/camera_photo.py
+++ src/bambi/bambi/scripts/optical_cam/camera_photo.py
@@ -11,27 +11,28 @@ from settings import camport
 
 def take_photo():
   srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  srv.settimeout(2)
+  srv.settimeout(5)
   rospy.loginfo('Trying to connect with yiCam @ ' + str(camaddr) + ':' + str(camport))
   srv.connect((camaddr, camport))
 
   srv.send('{"msg_id":257,"token":0}')
 
   data = srv.recv(512)
-  ropsy.loginfo('Got data from yiCam: ' + str(data))
+  rospy.loginfo('Got first data from yiCam, looking for "rval": ' + str(data))
   if "rval" in data:
-  	token = re.findall('"param": (.+) }',data)[0]	
+  	token = re.findall('"param":\s*(.+)\s*}',data)[0]	
   else:
   	data = srv.recv(512)
+	rospy.loginfo('Got second data from yiCam, looking for "rval": ' + str(data))
   	if "rval" in data:
-  		token = re.findall('"param": (.+) }',data)[0]	
+  		token = re.findall('"param":\s*(.+)\s*}',data)[0]	
 
-  ropsy.loginfo('Got data from yiCam: ' + str(data))
+  rospy.loginfo('Got data from yiCam: ' + str(data))
 
   tosend = '{"msg_id":769,"token":%s}' %token
   srv.send(tosend)
   data = srv.recv(512)
-  ropsy.loginfo('Got data from yiCam: ' + str(data))
+  rospy.loginfo('Got data from yiCam: ' + str(data))
 
 
   findCollection = list()
@@ -41,7 +42,7 @@ def take_photo():
 
   while len(findCollection) == 0 and i < maxTries:
     data = srv.recv(512)
-    ropsy.loginfo('Got data from yiCam: ' + str(data))
+    rospy.loginfo('Got data from yiCam: ' + str(data))
     findCollection = re.findall('"param":"/tmp/fuse_d/(.+)"}', data)
     ++i
 
@@ -57,6 +58,6 @@ def take_photo():
   fileName = '$BAMBI_OWNCLOUD_HOME/yi-cam-pic-' + datetime.datetime.now().strftime('%Y-%m-%d-%H:%M:%S') + '.jpg'
   fileName = os.path.expandvars(fileName)
 
-  ropsy.log(url)
+  rospy.loginfo(url)
   urllib.urlretrieve(url, filename=fileName)
   return fileName
